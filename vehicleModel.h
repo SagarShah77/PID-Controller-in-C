1ca8eafc51fa30e60c62cf190ee59820e020c3eb
Sagar Shah <shah7sagar7@gmail.com>
2018-02-23 08:24:54 -0800

    Header file for main program

---
+++ vehicleModel.h
@@ -0,0 +1,9 @@
+#define number_IError    4;      // number of integration terms for IError[n]
+#define Kp				300;     // gain proportional
+#define Kd              600;     // gain derivative
+#define ki              400;     // gain integral
+#define alpha1          1.0;   // weighting for kp
+#define alpha2          1.0;   // weighting for ki
+#define alpha3          1.0;   // weighting for kd
+#define minError        -1000.0; // min error limit
+#define maxError        1000.0; // max error limit
